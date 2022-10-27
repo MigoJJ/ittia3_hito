6628719376610f9abdba84f8c9a2d0d418569381
migowj <jjkoh21@gmail.com>
2022-10-27 11:43:48 +0900

    1

--- src/lanuarius/la_comm/Enter.java
+++ src/lanuarius/la_comm/Enter.java
@@ -11,7 +11,7 @@ public class Enter {
 	public static void main(String[] args){
 		System.out.println("Continue the program ? : ");
 		System.out.println(" Migo JJ");
-		DisplayButtonMessage.main(args);
+		DisplayButtonMessage.main(null);
     }
 }
 
