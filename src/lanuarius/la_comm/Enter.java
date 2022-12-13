2cfe456e2c86ee046dbdc7a701998ce3a2061178
migoey <migoey@migoey-vm>
2022-12-13 22:47:02 +0900

    "/home/migowj/ittia1_chart/

--- src/lanuarius/la_comm/Enter.java
+++ src/lanuarius/la_comm/Enter.java
@@ -6,9 +6,9 @@ public class Enter {
 // ------------------------------------------------------user directory definition
 	public static String currentUsersHomeDir = System.getProperty("user.dir" );
 	public static String wd = (currentUsersHomeDir);
-	public static String jwd = ("/home/migoey/ittia1_chart/je/panse/doro/text/samsara");
-//	public static String jwd = ("/home/migowj/ittia1_chart/je/panse/doro/text/samsara");
-	
+//	public static String jwd = ("/home/migoey/ittia1_chart/je/panse/doro/text/samsara");
+	public static String jwd = ("/home/migowj/ittia1_chart/je/panse/doro/text/samsara");
+		
 	public static void main(String[] args){
 		System.out.println("Continue the program ? : ");
 		System.out.println(" Migo JJ  wd  " + wd);
